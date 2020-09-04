1b615ecc07e983c6ccc34fd09655f783d209eaa8
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-04 17:59:29 +0700

    add button

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // all instance properties of class have to init
+    var flipCount: Int = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🤢", on: sender)
     }
-
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1) // typing Color, and double click Color Literal
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 
 }
 
